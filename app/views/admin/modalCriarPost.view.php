98d005e49d6f4524353cd021ada12bd63d4a64f8
ViniCampos12 <viniciuspriamo@gmail.com>
2024-11-16 17:29:27 -0300

    Ajustando um js

--- app/views/admin/modalCriarPost.view.php
+++ app/views/admin/modalCriarPost.view.php
@@ -31,4 +31,16 @@
     </div>
 </body>
     <script src="https://kit.fontawesome.com/654def639f.js" crossorigin="anonymous"></script>
+    <script>
+        const painel = document.querySelector('.imagemCriar');
+    const escrito = painel.querySelector('span');
+            // console.log(span1)
+            // console.log(label1);
+    const inputImagem = document.getElementById('adcImage');
+            // console.log(inputImagem);
+    inputImagem.addEventListener('change', function(){
+                escrito.innerHTML = "Imagem selecionada"; 
+                painel.style.color = "#88c8ee";
+})
+    </script>
 </html>
--- public/js/modal.js
+++ public/js/modal.js
@@ -11,13 +11,4 @@ function fechaModal() {
     document.querySelector(`.overlay`).style.display = 'none';
 }
 
-const painel = document.querySelector('.imagemCriar');
-const escrito = painel.querySelector('span');
-        // console.log(span1)
-        // console.log(label1);
-const inputImagem = document.getElementById('adcImage');
-        // console.log(inputImagem);
-inputImagem.addEventListener('change', function(){
-            escrito.innerHTML = "Imagem selecionada"; 
-            painel.style.color = "#88c8ee";
-})
+    
