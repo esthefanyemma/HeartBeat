65c43f4701e69f40172deab7be6f19b8333d56b0
JoseSimoes <josesimoesk10@gmail.com>
2024-11-27 23:07:01 -0300

    Corrige posicionamento do modal criar

--- app/views/admin/modalCriarUser.view.php
+++ app/views/admin/modalCriarUser.view.php
@@ -11,73 +11,75 @@
 </head>
 
 <body>
-    <div class="modaledit-CU modal" id="criarUser">
-        <form id="criarUser" action="usuarios/criar" method="post" enctype="multipart/form-data">
-            <div class="img-entrada-CU">
-                <label>
-                    <div class="escuro"><img src="../../../public/assets/pencil-alt.svg" alt=""></div>
-                    <input type="file" accept="image/*" name="imagem" class="img-inp-CU">
-                    <div class="containerProfileImg">
-                        <img class="profileImg" src="../../../public/assets/profile-picture-973460_1280.webp" alt="">
+    <main class="mainModalCriaUser">
+        <div class="modalCriar-CU modal" id="criarUser">
+            <form id="criarUser" action="usuarios/criar" method="post" enctype="multipart/form-data">
+                <div class="img-entrada-CU">
+                    <label>
+                        <div class="escuro"><img src="../../../public/assets/pencil-alt.svg" alt=""></div>
+                        <input type="file" accept="image/*" name="imagem" class="img-inp-CU">
+                        <div class="containerProfileImg">
+                            <img class="profileImg" src="../../../public/assets/profile-picture-973460_1280.webp" alt="">
+                        </div>
+                    </label>
+                </div>
+                <header class="titulo-CU">
+                </header>
+                <div class="info-usuario-CU">
+                    <div class="campoEntrada-CU">
+                        <div class="formularios-CU">
+                            <label class="nomecampos-CU">Nome:</label>
+                            <input type="text" name="nome" placeholder="HeartBeats Da Silva" required>
+                        </div>
+                        <div class="formularios-CU">
+                            <label class="nomecampos-CU">Email:</label>
+                            <input type="email" name="email" placeholder="heartbeats@exemplo.com" required>
+                        </div>
+                        <div class="formularios-CU">
+                            <label class="nomecampos-CU">Senha:</label>
+                            <input type="password" name="senha" placeholder="***********" required>
+                        </div>
                     </div>
-                </label>
-            </div>
-            <header class="titulo-CU">
-            </header>
-            <div class="info-usuario-CU">
-                <div class="campoEntrada-CU">
-                    <div class="formularios-CU">
-                        <label class="nomecampos-CU">Nome:</label>
-                        <input type="text" name="nome" placeholder="HeartBeats Da Silva" required>
-                    </div>
-                    <div class="formularios-CU">
-                        <label class="nomecampos-CU">Email:</label>
-                        <input type="email" name="email" placeholder="heartbeats@exemplo.com" required>
-                    </div>
-                    <div class="formularios-CU">
-                        <label class="nomecampos-CU">Senha:</label>
-                        <input type="password" name="senha" placeholder="***********" required>
+                    <div class="btns-edit-CU">
+                        <button type="submit">Criar</button>
+                        <button type="button" onclick="fechaModal()">Cancelar</button>
                     </div>
                 </div>
-                <div class="btns-edit-CU">
-                    <button type="submit">Criar</button>
-                    <button type="button" onclick="fechaModal()">Cancelar</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    <script src="https://kit.fontawesome.com/654def639f.js" crossorigin="anonymous"></script>
-    <script src="/public/js/modal.js"></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const painel = document.querySelector('.img-entrada-CU');
-            const inputImagem = painel.querySelector('input[type="file"]');
-            const spanImage = painel.querySelector('.profileImg');
-            const icone = painel.querySelector('.escuro img');
+            </form>
+        </div>
+        <script src="https://kit.fontawesome.com/654def639f.js" crossorigin="anonymous"></script>
+        <script src="/public/js/modal.js"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const painel = document.querySelector('.img-entrada-CU');
+                const inputImagem = painel.querySelector('input[type="file"]');
+                const spanImage = painel.querySelector('.profileImg');
+                const icone = painel.querySelector('.escuro img');
 
-            if (inputImagem) {
-                inputImagem.addEventListener('change', function(e) {
-                    const inputTarget = e.target;
-                    const arquivo = inputTarget.files[0];
+                if (inputImagem) {
+                    inputImagem.addEventListener('change', function(e) {
+                        const inputTarget = e.target;
+                        const arquivo = inputTarget.files[0];
 
-                    if (arquivo) {
-                        const reader = new FileReader();
-                        reader.addEventListener('load', function(e) {
-                            const readerTarget = e.target;
+                        if (arquivo) {
+                            const reader = new FileReader();
+                            reader.addEventListener('load', function(e) {
+                                const readerTarget = e.target;
 
-                            spanImage.src = readerTarget.result;
-                            icone.style.display = 'none';
-                        });
+                                spanImage.src = readerTarget.result;
+                                icone.style.display = 'none';
+                            });
 
-                        reader.readAsDataURL(arquivo);
-                    } else {
-                        spanImage.src = "../../../public/assets/profile-picture-973460_1280.webp";
-                        icone.style.display = 'block';
-                    }
-                });
-            }
-        });
-    </script>
+                            reader.readAsDataURL(arquivo);
+                        } else {
+                            spanImage.src = "../../../public/assets/profile-picture-973460_1280.webp";
+                            icone.style.display = 'block';
+                        }
+                    });
+                }
+            });
+        </script>
+    </main>
 </body>
 
 </html>
