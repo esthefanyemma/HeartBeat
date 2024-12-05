83ea2c42adf614cd0e0f7d77c54d4342fb1d8479
JoseSimoes <josesimoesk10@gmail.com>
2024-12-05 11:39:50 -0300

    Implementa paginacao na tabela de Publicacoes

--- app/Controllers/PostController.php
+++ app/Controllers/PostController.php
@@ -10,10 +10,34 @@ class PostController
 
     public function index()
     {
-        $posts = App::get('database')->selectAll('posts'); 
-        $users = App::get('database')->selectAll('users');
 
-        return view('admin/tabelaPubli', compact('posts', 'users')); 
+         $page=1;
+         if(isset($_GET['paginacaoNumero']) && !empty($_GET['paginacaoNumero'])){
+             $page = intval($_GET['paginacaoNumero']);
+             if($page <= 0){
+                 return redirect('admin/posts');
+             }
+         }
+         $itensPage = 5;
+         $inicio = $itensPage * $page - $itensPage;
+         $rows_count = App::get('database')->countAll('posts');
+
+         if($inicio > $rows_count){
+             return redirect('admin/posts');
+         }
+        // $users = App::get('database')->selectAll('users', $inicio, $itensPage);
+        // $total_pages = ceil($rows_count / $itensPage);
+
+        // return view('admin/tabelaUsuario',compact('users','page', 'total_pages') );
+
+
+        $total_pages = ceil($rows_count / $itensPage);
+
+        $posts = App::get('database')->selectAll('posts', $inicio, $itensPage); 
+        $users = App::get('database')->selectAll('users', $inicio, $itensPage);
+
+
+        return view('admin/tabelaPubli', compact('posts', 'users', 'page', 'total_pages')); 
     }
 
     public function create()
--- app/views/admin/tabelaPubli.view.php
+++ app/views/admin/tabelaPubli.view.php
@@ -1,107 +1,110 @@
  <!DOCTYPE html>
-<html lang="pt">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Página de Publicações</title>
-    <link rel="stylesheet" href="/public/css/publicacoesStyle.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Itim&display=swap"
-      rel="stylesheet"
-    />
-  </head>
-  <body>
-    <div class="overlay" onclick="fechaModal()"></div>
-    <main>
-      <div class="mainTabela">
-        <div class="apresentacao">
-          <div class="logo">
-            <img src="/public/assets/HeartBeats_img.png" alt="Logo HeartBeats" />
-            <h2>Tabela de Publicações</h2>
-          </div>
-          <div class="btn" onclick="abrirModal('modalCriarPost')">
-            <p>Criar</p>
-            <i class="fa-solid fa-plus"></i>
-            <?php foreach($users as $user): ?>
-            <?php require(__DIR__.'/../admin/modalCriarPost.view.php');?>
-            <?php endforeach?>
-          </div>
-        </div>
-        <div class="container-usuario">
-          <table>
-            <thead>
-              <div class="container-busca">
-                <div id="divBuscaPubli">
-                  <input type="text" id="txtBuscaPubli" placeholder="Pesquisar" />
-                  <i class="fa-solid fa-magnifying-glass"></i> 
-                </div>
-              </div>
-              <tr style="border-bottom: 2px solid #bd7bd1">
-                <th class="id">ID</th>
-                <th class="titulo">Título</th>
-                <th class="autor">Autor</th>
-                <th class="data">Data de criação</th>
-                <th class="acoes-tr">Ações</th>
-              </tr>     
-            </thead>
-            <tbody>
-              <?php foreach($posts as $post):?>
-              <tr>
-                <td class="id"><?php echo $post->id; ?></td>
-                <td class="titulo"><p><?php echo $post->title; ?></p></td>
-                <td class="autor"><?php foreach($users as $user): ?><?= $post->author == $user->id ? $user->name : "" ?><?php endforeach ?></td> <!-- Tem que ver melhor isso aqui para aparecer o nome do autor e não o id -->         
-                <!-- <td class="data"><?php echo $post->date; ?></td> -->
-                <td class="data"><?php 
-                    $horario = new DateTime($post->date);
-                    echo $horario->format('d/m/y');
-                ?></td>
-                <td class="acoes-tr">
-                  <div class="acoes">
-                    <div class="visualizar" onclick="abrirModal('modalVerPost<?= $post->id?>')">
-                      <i class="fa-solid fa-eye"></i>
-                    </div>
-                    <div class="editar" onclick="abrirModal('modalEditPost<?= $post->id?>')">
-                      <i class="fa-solid fa-pencil"></i>
-                    </div>
-                    <div class="deletar" onclick="abrirModal('cancelar2<?= $post->id?>')">
-                      <i class="fa-solid fa-trash"></i>
-                    </div>
-                  </div>
-                </td>
+ <html lang="pt">
 
-                  <!-- Modais -->
-                   <?php foreach($users as $user):?>
-                  <?php require(__DIR__.'/../admin/modalVisualizarPost.view.php');?>
-                  <?php require(__DIR__.'/../admin/modalEditarPost.view.php');?>
-                  <?php endforeach?>
-                  <?php require(__DIR__.'/../admin/modalDeletarPost.view.php');?>
-                  
+ <head>
+   <meta charset="UTF-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+   <title>Página de Publicações</title>
+   <link rel="stylesheet" href="/public/css/publicacoesStyle.css">
+   <link rel="preconnect" href="https://fonts.googleapis.com" />
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+   <link
+     href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&display=swap"
+     rel="stylesheet" />
+   <link rel="preconnect" href="https://fonts.googleapis.com" />
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+   <link
+     href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Itim&display=swap"
+     rel="stylesheet" />
+ </head>
 
-                
-              </tr>
-              <?php endforeach;?>
-            </tbody>
-          </table>
-          <div class="paginacao">
-            <button onclick="changePage(-1)" class="seta">←</button>
-            <span class="page-number"><button class="button">1</button></span>
-            <span class="page-number"><button class="button">2</button></span>
-            <span class="page-number"><button class="button">3</button></span>
-            <button onclick="changePage(1)" class="seta">→</button>
-          </div>
-        </div>
-      </div>
-      </div>
-    </main>
-  </body>
-  <script src="https://kit.fontawesome.com/654def639f.js"crossorigin="anonymous"></script>
-  <script src="/public/js/modal.js"></script>
-</html>
+ <body>
+   <div class="overlay" onclick="fechaModal()"></div>
+   <main>
+     <div class="mainTabela">
+       <div class="apresentacao">
+         <div class="logo">
+           <img src="/public/assets/HeartBeats_img.png" alt="Logo HeartBeats" />
+           <h2>Tabela de Publicações</h2>
+         </div>
+         <div class="btn" onclick="abrirModal('modalCriarPost')">
+           <p>Criar</p>
+           <i class="fa-solid fa-plus"></i>
+           <?php foreach ($users as $user): ?>
+             <?php require(__DIR__ . '/../admin/modalCriarPost.view.php'); ?>
+           <?php endforeach ?>
+         </div>
+       </div>
+       <div class="container-usuario">
+         <table>
+           <thead>
+             <div class="container-busca">
+               <div id="divBuscaPubli">
+                 <input type="text" id="txtBuscaPubli" placeholder="Pesquisar" />
+                 <i class="fa-solid fa-magnifying-glass"></i>
+               </div>
+             </div>
+             <tr style="border-bottom: 2px solid #bd7bd1">
+               <th class="id">ID</th>
+               <th class="titulo">Título</th>
+               <th class="autor">Autor</th>
+               <th class="data">Data de criação</th>
+               <th class="acoes-tr">Ações</th>
+             </tr>
+           </thead>
+           <tbody>
+             <?php foreach ($posts as $post): ?>
+               <tr>
+                 <td class="id"><?php echo $post->id; ?></td>
+                 <td class="titulo">
+                   <p><?php echo $post->title; ?></p>
+                 </td>
+                 <td class="autor"><?php foreach ($users as $user): ?><?= $post->author == $user->id ? $user->name : "" ?><?php endforeach ?></td> <!-- Tem que ver melhor isso aqui para aparecer o nome do autor e não o id -->
+                 <!-- <td class="data"><?php echo $post->date; ?></td> -->
+                 <td class="data"><?php
+                                  $horario = new DateTime($post->date);
+                                  echo $horario->format('d/m/y');
+                                  ?></td>
+                 <td class="acoes-tr">
+                   <div class="acoes">
+                     <div class="visualizar" onclick="abrirModal('modalVerPost<?= $post->id ?>')">
+                       <i class="fa-solid fa-eye"></i>
+                     </div>
+                     <div class="editar" onclick="abrirModal('modalEditPost<?= $post->id ?>')">
+                       <i class="fa-solid fa-pencil"></i>
+                     </div>
+                     <div class="deletar" onclick="abrirModal('cancelar2<?= $post->id ?>')">
+                       <i class="fa-solid fa-trash"></i>
+                     </div>
+                   </div>
+                 </td>
+
+                 <!-- Modais -->
+                 <?php foreach ($users as $user): ?>
+                   <?php require(__DIR__ . '/../admin/modalVisualizarPost.view.php'); ?>
+                   <?php require(__DIR__ . '/../admin/modalEditarPost.view.php'); ?>
+                 <?php endforeach ?>
+                 <?php require(__DIR__ . '/../admin/modalDeletarPost.view.php'); ?>
+
+
+
+               </tr>
+             <?php endforeach; ?>
+           </tbody>
+         </table>
+         <div class="paginacao">
+           <a href="?paginacaoNumero=<?= $page - 1 ?>" class="seta <?= $page <= 1 ? "disabled" : "" ?> ">←</a>
+           <?php for ($page_number = 1; $page_number <= $total_pages; $page_number++): ?>
+             <span class="page-number <?= $page_number == $page ? "active" : "" ?>"><a class="button"
+                 href="?paginacaoNumero=<?= $page_number ?>"><?= $page_number ?> </a></span>
+           <?php endfor ?>
+           <a href="?paginacaoNumero=<?= $page + 1 ?>" class="seta <?= $page >= $total_pages ? 'disabled' : "" ?> ">→</a>
+         </div>
+       </div>
+     </div>
+   </main>
+ </body>
+ <script src="https://kit.fontawesome.com/654def639f.js" crossorigin="anonymous"></script>
+ <script src="/public/js/modal.js"></script>
+
+ </html>
